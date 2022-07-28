09015d52b4cf02251a412c54bbee01805ff1d4df
tj-bu <tj-bu@DESKTOP-E31N4N2>
2022-07-28 16:55:05 +0900

    second git test

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -9,11 +9,11 @@ public class GitTest {
 		System.out.println("구구단");
 		
 		for(int i = 2; i < 10; i++) {
-			System.out.printf("---%d 단--- \n", i);
+			System.out.printf("\n---%d 단--- \n", i);
 			for(int j = 1; j < 10; j++) {
 				System.out.printf("%d X %d = %d \n", i, j, i*j);
 			}
 		}
-		
 	}
+
 }
