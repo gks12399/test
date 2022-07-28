9a2a9ae71c968bfa76375e5dee7ca3b4683b1612
tj-bu <tj-bu@DESKTOP-E31N4N2>
2022-07-28 16:47:38 +0900

    first git test

---
+++ GitTest/src/GitTest.java
@@ -0,0 +1,19 @@
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello JAVA!");
+		
+		System.out.println("----------------------------");
+		System.out.println("구구단");
+		
+		for(int i = 2; i < 10; i++) {
+			System.out.printf("---%d 단--- \n", i);
+			for(int j = 1; j < 10; j++) {
+				System.out.printf("%d X %d = %d \n", i, j, i*j);
+			}
+		}
+		
+	}
+}
