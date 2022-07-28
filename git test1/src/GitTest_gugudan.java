2d7945f866488d68febe3e6bb9795e15a751f47a
USER <USER@192.168.219.100>
2022-07-28 22:33:29 +0900

    home - git test

---
+++ git test1/src/GitTest_gugudan.java
@@ -0,0 +1,14 @@
+
+public class GitTest_gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for( int i = 2; i <10; i++) {
+			System.out.printf("----%d 단---- \n", i);
+			for( int j = 1; j < 10; j++) {
+				System.out.printf("%d X %d = %d \n", i, j, (i * j));
+			}
+		}
+	}
+}
