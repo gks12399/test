bd7603d24f89da4b88ed29cbc5afcc287af90142
tj-bu <tj-bu@DESKTOP-98E38CS>
2022-07-28 18:05:27 +0900

    ㅋ ㅋㅋ 후후 헤헤 하하하하하히호헤호

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -12,7 +12,7 @@ public class GitTest {
 			System.out.printf("---%d 단--- \n", i);
 			for(int j = 1; j < 10; j++) {
 				System.out.printf("%d X %d = %d \n", i, j, i*j);
-				테러당
+				ㅋㅋㅋㅋ하하하헤헤헤
 			}
 		}
 	}
